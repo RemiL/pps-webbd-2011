7e606c2e5104078aecb706cdb260df1c8f03a8d2
fabien.bini <fabien.bini@8e272b94-ffaf-4885-f8fc-8a814f68a3e8>
2011-03-22 15:55:00 +0000

    fermeture des onglets (marche) bouton fermer bien placé div bien positionnés entre eux

--- trunk/js/initialisation.js
+++ trunk/js/initialisation.js
@@ -1,17 +1,14 @@
-$(function() {
-			$( "#action" ).tabs();
+$(function() { 
+			$("#action").tabs();
 			$("#postit").button();
 		});
 
-function fermerOnglet(index)
+function fermerOnglet(onglet)
 {
+	var index = Number(onglet.id);
 	for(var i=index+1; i<$( "#action" ).tabs( "length" ); i++)
 	{
-		var id = 'ongletTache'+i;
-		var id2 = i - 1;
-		var tab = document.getElementById(id);
-		tab.id = 'ongletTache'+id2;
-		tab.onclick = "fermerOnglet(" + id2 + ");";
+		document.getElementById(i).id = i - 1;
 	}
 
 	$( "#action" ).tabs( "remove" , index );
