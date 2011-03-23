26caf341a4f295c669b624b2a73e9ffa7e2177c5
groupeCMT07@gmail.com <groupeCMT07@gmail.com@8e272b94-ffaf-4885-f8fc-8a814f68a3e8>
2011-03-23 16:09:44 +0000

    Ouverture/fermeture des onglets Ouverture/fermeture du panneau de post-it Ebauche de post-it (draggable, boutons)

--- js/initialisation.js
+++ js/initialisation.js
@@ -1,14 +1,51 @@
 $(function() { 
-			$("#action").tabs();
-			$("#postit").button();
+			$("#action").tabs({ tabTemplate: '<li>#{label}</li>' });
+			$("#afficherPostit").button({ icons: {primary:'ui-icon-circle-triangle-n'}, text: false });
+			$("#ajouterOnglet").button({ icons: {primary:'ui-icon-plusthick'}, text: false });
+			$("#ajouterOnglet").click(function() { addTab(); });
+			$("#afficherPostit").click(function() { afficherCacherPanneauPostit(); });
+			$( "#postit1" ).draggable({ containment: 'parent' }, { scroll: false });
 		});
 
+var panneauPostitOuvert = false;
+		
+function afficherCacherPanneauPostit()
+{
+	if(!panneauPostitOuvert)
+	{
+		$("#panneauPostit").switchClass( "panneauPostitFerme", "panneauPostitOuvert", 0 );
+		panneauPostitOuvert = true;
+		$("#afficherPostit").button( "option", "icons", {primary:'ui-icon-circle-triangle-s'} );
+	}
+	else
+	{
+		$("#panneauPostit").switchClass( "panneauPostitOuvert", "panneauPostitFerme", 0 );
+		panneauPostitOuvert = false;
+		$("#afficherPostit").button( "option", "icons", {primary:'ui-icon-circle-triangle-n'} );
+	}
+}	
+
+function addTab()
+{
+	var index = $("#action").tabs( "length" );
+	var ajouterOnglet = document.getElementById('ajouterOnglet');
+	document.getElementById('listeOnglets').removeChild(ajouterOnglet);
+	
+	$("#action").tabs( "add" , "#newTask"+index, '<div id="i' + index + '" class="fermerOnglet" onclick="fermerOnglet(this);"><span class="ui-icon ui-icon-closethick"></span></div><a href="#newTask' + index + '" class="titreOnglet" title="New task">New task</a>', index );
+	$("#action").tabs( "select" ,  index );
+	
+	document.getElementById('newTask'+index).innerHTML = 'New task'+index;
+	
+	ajouterOnglet.className = 'ui-button ui-widget ui-state-default ui-corner-all ui-button-icon-only';
+	document.getElementById('listeOnglets').appendChild(ajouterOnglet);
+}
+		
 function fermerOnglet(onglet)
 {
-	var index = Number(onglet.id);
+	var index = Number(onglet.id.charAt(1));
 	for(var i=index+1; i<$( "#action" ).tabs( "length" ); i++)
 	{
-		document.getElementById(i).id = i - 1;
+		document.getElementById('i'+i).id = 'i' + (i - 1);
 	}
 
 	$( "#action" ).tabs( "remove" , index );
