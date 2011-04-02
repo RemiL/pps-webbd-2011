795c6d5e5ef40201c2bb2ca56b0476b8fa6a6ea3
groupeCMT07@gmail.com <groupeCMT07@gmail.com@8e272b94-ffaf-4885-f8fc-8a814f68a3e8>
2011-04-02 14:05:17 +0000

    Le déplacement des box fonctionne
    -déplacer les box les unes par rapport aux autres
    -se déplacer dans les box
    -supprimer les box

--- js/initialisation.js
+++ js/initialisation.js
@@ -165,9 +165,47 @@ function ajouterTacheBox(box)
 	alert("ajouter tache box " + box.id);
 }
 
-function supprimerBox(box)
+function supprimerBox(box) 
 {
-	document.getElementById("listeBox").removeChild(box);
+    var index = Number(box.id.substring(1, box.id.length));
+
+    if (document.getElementById('b1').offsetLeft < 0) 
+    {
+        index--;
+    }
+    else
+    {
+        index++;
+    }
+
+    if (document.getElementById('b' + index) != null)
+    {
+        if (index < Number(box.id.substring(1, box.id.length)))
+        {
+            while (index > 0)
+            {
+                $("#b" + index).animate({ marginLeft: '+=207px' }, 'slow');
+                index--;
+            }
+        }
+        else 
+        {
+            while (document.getElementById('b' + index) != null) 
+            {
+                $("#b" + index).animate({ marginLeft: '-=207px' }, 'slow');
+                index++;
+            }
+        }
+    }
+
+    index = Number(box.id.substring(1, box.id.length)) + 1;
+    while (document.getElementById('b' + index) != null) 
+    {
+        document.getElementById('b' + index).id = 'b' + (index - 1);
+        index++;
+    }
+
+    document.getElementById("listeBox").removeChild(box);
 }
 
 function boxVersHaut(box)
@@ -186,9 +224,9 @@ function boxVersGauche(box)
 	if(document.getElementById('b'+index) != null)
 	{
 		box.style.zIndex = 2;
-		$( "#"+box.id ).animate({marginLeft: '-=206px'}, 'slow');
+		$( "#"+box.id ).animate({marginLeft: '-=207px'}, 'slow');
 		document.getElementById('b'+index).style.zIndex = 1;
-		$( "#b" + index ).animate({marginLeft: '+=206px'}, 'slow');
+		$( "#b" + index ).animate({marginLeft: '+=207px'}, 'slow');
 		document.getElementById('b'+index).id = box.id;
 		box.id = 'b' + index;
 	}
@@ -200,9 +238,9 @@ function boxVersDroite(box)
 	if(document.getElementById('b'+index) != null)
 	{
 		box.style.zIndex = 2;
-		$( "#"+box.id ).animate({marginLeft: '+=206px'}, 'slow');
+		$( "#"+box.id ).animate({marginLeft: '+=207px'}, 'slow');
 		document.getElementById('b'+index).style.zIndex = 1;
-		$( "#b" + index ).animate({marginLeft: '-=206px'}, 'slow');
+		$( "#b" + index ).animate({marginLeft: '-=207px'}, 'slow');
 		document.getElementById('b'+index).id = box.id;
 		box.id = 'b' + index;
 	}
@@ -210,10 +248,41 @@ function boxVersDroite(box)
 
 function versGaucheListeBox() 
 {
-    alert("déplacer la liste vers la gauche");
+    if (document.getElementById('b1').offsetLeft % 207 == 0)
+    {
+        var index = 1;
+
+        while(document.getElementById('b' + index) != null) 
+        {
+            index++;
+        }
+
+        if (document.getElementById('b' + (index - 1)).offsetLeft + 200 > document.getElementById('listeBox').offsetWidth)
+        {
+            index = 1;
+
+            while(document.getElementById('b' + index) != null)
+            {
+                $("#b" + index).animate({ marginLeft: '-=207px' }, 'slow');
+                index++;
+            }
+        }
+    }
 }
 
 function versDroiteListeBox()
 {
-    alert("déplacer la liste vers la droite");
+    if (document.getElementById('b1').offsetLeft % 207 == 0) 
+    {
+        if (document.getElementById('b1').offsetLeft < 0)
+        {
+            var index = 1;
+
+            while (document.getElementById('b' + index) != null) 
+            {
+                $("#b" + index).animate({ marginLeft: '+=207px' }, 'slow');
+                index++;
+            }
+        }
+    }
 }
