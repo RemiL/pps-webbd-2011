e8fbca39f4604f1fefff92d3a8a9ae6e40188d90
remi.lacroix <remi.lacroix@8e272b94-ffaf-4885-f8fc-8a814f68a3e8>
2011-04-02 19:56:31 +0000

    Ajout : le calendrier est maintenant rempli avec les tâches du jour.
    Todo : Améliorer le design et ajouter la possibilité de changer de jour.

---
+++ trunk/js/initialisation_gapi.js
@@ -0,0 +1,112 @@
+google.load("gdata", "2.s");
+// On laisse le temps au cookie d'être enregistré
+google.setOnLoadCallback(function() { setTimeout(remplirCalendrierDuJour, 500); });
+
+var calendarService, scope;
+
+function connecterCalendar()
+{
+    calendarService = new google.gdata.calendar.CalendarService("PPS-OneThingAtATime-0.01");
+    
+    if (!google.accounts.user.checkLogin(scope))
+    {
+        scope = "https://www.google.com/calendar/feeds/";
+        var token = google.accounts.user.login(scope);
+    }
+}
+
+// Gestionnaire d'erreur
+function gestErreur(erreur)
+{
+    alert(erreur);
+}
+
+function getEvenementsDuJour(callback)
+{
+    connecterCalendar();
+    
+    // On veut récupérer tous les événements depuis le calendrier par défaut
+    var feedUri = "https://www.google.com/calendar/feeds/default/private/full";
+    
+    // Préparation de la requête
+    var query = new google.gdata.calendar.CalendarEventQuery(feedUri);
+    
+    // On souhaite récupérer les événements du jour
+    var today_start = new Date();
+    today_start.setHours(0);
+    today_start.setMinutes(0);
+    today_start.setSeconds(0);
+    today_start.setMilliseconds(0);
+    var startMin = new google.gdata.DateTime(today_start);
+    var today_end = new Date();
+    today_end.setHours(23);
+    today_end.setMinutes(59);
+    today_end.setSeconds(59);
+    today_end.setMilliseconds(999);
+    var startMax = new google.gdata.DateTime(today_end);
+    query.setMinimumStartTime(google.gdata.DateTime.toIso8601(startMin));
+    query.setMaximumStartTime(google.gdata.DateTime.toIso8601(startMax));
+    
+    // Envoi de la requête
+    calendarService.getEventsFeed(query, callback, gestErreur);
+}
+
+function getHoraire(date)
+{
+    var horaire = date.getHours()+'h';
+    
+    if (date.getHours() < 10)
+        horaire = '0'+horaire;
+    if (date.getMinutes() < 10)
+        horaire = horaire+'0';
+        
+    return horaire+date.getMinutes();
+}
+
+function remplirCalendrier(root)
+{
+    // Le calendrier sur la page
+    var calendrier = document.getElementById("calendar");
+    
+    // Les entrées à afficher supposées concerner un seul jour.
+    var entrees = root.feed.getEntries();
+
+    if (entrees.length > 0)
+    {
+        // Une tâche dans le calendrier
+        var tache;
+        // L'horaire de la tache
+        var horaire;
+        // Le titre de la tache
+        var titre;
+        
+        for (var i = 0; i < entrees.length; i++)
+        {
+            var entree = entrees[i];
+            
+            tache = document.createElement('div');
+            
+            tache.className = "tacheBox ui-corner-all";
+            tache.style.position = 'absolute';
+            // On calcule le placement à partir du nombre de minutes depuis minuit (1 pixel = 2 minutes)
+            tache.style.top = (entree.getTimes()[0].getStartTime().getDate().getHours()*60 + entree.getTimes()[0].getStartTime().getDate().getMinutes()) / 2 + 'px';
+            // On calcule la hauteur à partir de la durée
+            tache.style.height = (entree.getTimes()[0].getEndTime().getDate().getTime() - entree.getTimes()[0].getStartTime().getDate().getTime()) / (60*2*1000) + 'px';
+            
+            horaire = document.createElement('div');
+            horaire.innerHTML = getHoraire(entree.getTimes()[0].getStartTime().getDate())+' - '+getHoraire(entree.getTimes()[0].getEndTime().getDate())+ ' :';
+            tache.appendChild(horaire);
+            
+            titre = document.createElement('div');
+            titre.innerHTML = entree.getTitle().getText();
+            tache.appendChild(titre);
+            
+            calendrier.appendChild(tache);
+        }
+    }
+}
+
+function remplirCalendrierDuJour()
+{
+    getEvenementsDuJour(remplirCalendrier);
+}
