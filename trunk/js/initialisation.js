fb5dd094eb385e1dc2ff09cfba138f0b2a8767fd
remi.lacroix <remi.lacroix@8e272b94-ffaf-4885-f8fc-8a814f68a3e8>
2011-04-19 15:37:14 +0000

    Amélioration : le contenu des onglets n'est pas rechargé à chaque fois que l'onglet est sélectionné ce qui rend le formulaire utilisable.

--- trunk/js/initialisation.js
+++ trunk/js/initialisation.js
@@ -1,5 +1,5 @@
 ﻿$(function () {
-    $("#action").tabs({ closable: true, tabTemplate: '<li><a class="titreOnglet" href="#{href}">#{label}</a></li>' });
+    $("#action").tabs({ closable: true, tabTemplate: '<li><a class="titreOnglet" href="#{href}">#{label}</a></li>', cache: true });
     $("#afficherPostit").button({ icons: {primary:'ui-icon-circle-triangle-n'}, text: false });
     $("#ajouterOnglet").button({ icons: {primary:'ui-icon-plusthick'}, text: false });
     $("#ajouterOnglet").click(function() { addTab(); });
