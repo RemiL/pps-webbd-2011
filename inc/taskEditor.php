da68dbe81bd2a11c16c204efb9caf20c767b1212
groupeCMT07@gmail.com <groupeCMT07@gmail.com@8e272b94-ffaf-4885-f8fc-8a814f68a3e8>
2011-04-25 16:26:16 +0000

    Le menu d'action (une colonne avec des onglets)

--- inc/taskEditor.php
+++ inc/taskEditor.php
@@ -1,43 +1,45 @@
-<form name="taskEditor" class="taskEditor" <?php if (isset($_GET['id'])) echo "id=\"form_{$_GET['id']}\""; ?>>
-  <div>
-    <label for="title">Title</label><input name="title" type="text" />
-  </div>
-  <div>
-    <label for="beginDate">When</label>
-    <input name="beginDate" type="text" />
-    <input name="beginTime" type="text" />
-    to
-    <input name="endDate" type="text" />
-    <input name="endTime" type="text" />
-  </div>
-  <div>
-    <label for="location">Location</label><input name="location" type="text" />
-  </div>
-  <div>
-    <label for="priority">Priority</label>
-    <select name="priority">
-      <option value="5">very high</option>
-      <option value="4">high</option>
-      <option value="3" selected="selected">normal</option>
-      <option value="2">low</option>
-      <option value="1">very low</option>
-    </select>
-  </div>
-  <div>
-    <label for="dependancies">Dependancies</label> !TODO!
-  </div>
-  <div>
-<label for="activities">Activities</label> !TODO!
-  </div>
-  <div>
-    <label for="description">Description</label><div class="description"><textarea name="description"></textarea></div>
-  </div>
-<?php
-if (isset($_GET['id']))
-{
-    echo "<script type=\"text/javascript\">";
-    echo "Task.tasks['{$_GET['id']}'].fillEditor();";
-    echo "</script>";
-}
-?>
-</form>
+<div class="contentMenuAction">
+    <form name="taskEditor" class="taskEditor" <?php if (isset($_GET['id'])) echo "id=\"form_{$_GET['id']}\""; ?>>
+        <div>
+            <label for="title">Title</label><input name="title" type="text" />
+        </div>
+        <div>
+            <label for="beginDate">When</label>
+            <input name="beginDate" type="text" />
+            <input name="beginTime" type="text" />
+            to
+            <input name="endDate" type="text" />
+            <input name="endTime" type="text" />
+        </div>
+        <div>
+            <label for="location">Location</label><input name="location" type="text" />
+        </div>
+        <div>
+            <label for="priority">Priority</label>
+	        <select name="priority">
+		        <option value="5">very high</option>
+		        <option value="4">high</option>
+		        <option value="3" selected="selected">normal</option>
+		        <option value="2">low</option>
+		        <option value="1">very low</option>
+	        </select>
+        </div>
+        <div>
+	        <label for="dependancies">Dependancies</label> !TODO!
+        </div>
+        <div>
+	        <label for="activities">Activities</label> !TODO!
+        </div>
+        <div>
+	        <label for="description">Description</label><div class="description"><textarea name="description"></textarea></div>
+        </div>
+        <?php
+        if (isset($_GET['id']))
+        {
+	        echo "<script type=\"text/javascript\">";
+	        echo "Task.tasks['{$_GET['id']}'].fillEditor();";
+	        echo "</script>";
+        }
+        ?>
+    </form>
+</div>
--- js/initialisation.js
+++ js/initialisation.js
@@ -1,8 +1,14 @@
 ﻿$(function () {
-    $("#action").tabs({ closable: true, tabTemplate: '<li><a class="titreOnglet" href="#{href}">#{label}</a></li>', cache: true });
-    $("#afficherPostit").button({ icons: {primary:'ui-icon-circle-triangle-n'}, text: false });
-    $("#ajouterOnglet").button({ icons: {primary:'ui-icon-plusthick'}, text: false });
-    $("#ajouterOnglet").click(function() { addTab(); });
-    $("#afficherPostit").click(function() { afficherCacherPanneauPostit(); });
+    $("#action").tabs({ closable: true, tabTemplate: '<li><a class="titreOnglet" href="#{href}">#{label}</a></li>', cache: true, idPrefix: 'ui-tabs-action' });
+    
+    // Crée le bouton pour ajouter un onglet
+    var boutonAjouterOnglet = document.createElement("li");
+    boutonAjouterOnglet.id = "ajouterOnglet";
+    document.getElementById("listeOnglets").appendChild(boutonAjouterOnglet);
+
+    $("#afficherPostit").button({ icons: { primary: 'ui-icon-circle-triangle-n' }, text: false });
+    $("#ajouterOnglet").button({ icons: { primary: 'ui-icon-plusthick' }, text: false });
+    $("#ajouterOnglet").click(function () { addTab(); });
+    $("#afficherPostit").click(function () { afficherCacherPanneauPostit(); });
     $("#panneauPostit").dblclick(function (event) { addPostit(event); });
 });
