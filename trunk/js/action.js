9384aeab003e41d5671ec52919d3fc1a062102ec
groupeCMT07@gmail.com <groupeCMT07@gmail.com@8e272b94-ffaf-4885-f8fc-8a814f68a3e8>
2011-04-25 16:26:16 +0000

    Le menu d'action (une colonne avec des onglets)

--- trunk/js/action.js
+++ trunk/js/action.js
@@ -3,18 +3,37 @@ function addTab(title, id) {
         title = 'New task';
 
     var index = $("#action").tabs("length");
+
+    // Enlève le bouton pour ajouter un onglet
     var ajouterOnglet = document.getElementById('ajouterOnglet');
     document.getElementById('listeOnglets').removeChild(ajouterOnglet);
 
+    // Construit l'url de l'éditeur
     var url = 'inc/taskEditor.php';
     if (id != undefined)
         url += '?id=' + id;
 
-    $("#action").tabs('add', url, title);
+    // Construit le div contenant les actions possibles sur la tâche
+    var divContent = document.createElement("div");
+    divContent.id = "ui-tabs-action" + $("#action").tabs("length");
+    var ulContent = document.createElement("ul");
+    ulContent.className = "menuAction";
+    divContent.appendChild(ulContent);
+    $("#action").append(divContent);
+
+    // Ajoute l'onglet de la tâche
+    $("#action").tabs('add', "#"+divContent.id, title);
     $("#action").tabs("select", index);
 
+    // Remet le bouton pour ajouter un onglet
     ajouterOnglet.className = 'ui-button ui-widget ui-state-default ui-corner-all ui-button-icon-only';
     document.getElementById('listeOnglets').appendChild(ajouterOnglet);
+
+    // Crée l'onglet Edit
+    $("#" + divContent.id).tabs({ tabTemplate: '<li><a class="menuActionTab" href="#{href}">#{label}</a></li>', cache: true, idPrefix: 'ui-tabs-action-menu' });
+    $("#" + divContent.id).tabs("add", url, "Edit");
+    // JQuery crée 2 div dont un inutil, il faut l'enlever
+    $("#ui-tabs-action-menu" + $("#" + divContent.id).tabs("length")).detach();
 
     return index;
 }
