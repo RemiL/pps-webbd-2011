905a55c01c6fd7721f172f504dd125d46f50752f
nivet1@hotmail.com <nivet1@hotmail.com@8e272b94-ffaf-4885-f8fc-8a814f68a3e8>
2011-04-26 13:29:28 +0000

    - Création de mail.php et de son css dans taskeditor.css

--- inc/mail.php
+++ inc/mail.php
@@ -1,3 +1,25 @@
 <div class="contentMenuAction">
-    MAIL
+	<form name="taskEditor" class="taskEditor" <?php if (isset($_GET['id'])) echo "id=\"form_{$_GET['id']}\""; ?>>
+        <div class="recipient">
+            <label for="recipient">recipient</label><input name="recipient" type="text" />
+        </div>
+        <div class="object">
+            <label for="object">object</label><input name="object" type="text" />
+        </div>
+        <div>
+            <label for="body"></label><div class="body"><textarea name="body"></textarea></div>
+        </div>
+		<div class="button">
+			<input value="Send" type="button" />
+        </div>
+  
+        <?php
+        if (isset($_GET['id']))
+        {
+	        echo "<script type=\"text/javascript\">";
+	        echo "Task.tasks['{$_GET['id']}'].fillEditor();";
+	        echo "</script>";
+        }
+        ?>
+    </form>
 </div>
