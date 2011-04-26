9c67ea5cae0dc4155770e5b238fd46d7e1f95f51
groupeCMT07@gmail.com <groupeCMT07@gmail.com@8e272b94-ffaf-4885-f8fc-8a814f68a3e8>
2011-04-26 15:03:10 +0000

    taskEditor : il y a des champs en plus

--- trunk/inc/taskEditor.php
+++ trunk/inc/taskEditor.php
@@ -1,5 +1,5 @@
 <div class="contentMenuAction">
-    <form name="taskEditor" class="taskEditor" <?php if (isset($_GET['id'])) echo "id=\"form_{$_GET['id']}\""; ?>>
+    <form name="taskEditor" class="taskEditor" <?php if (isset($_GET['id'])) echo "id=\"form_{$_GET['id']}\""; ?> onsubmit="return false;">
         <div>
             <label for="title">Title</label><input name="title" type="text" />
         </div>
@@ -28,18 +28,52 @@
 	        <label for="dependancies">Dependancies</label> !TODO!
         </div>
         <div>
-	        <label for="activities">Activities</label> !TODO!
+	        <label for="activities">Activities</label> <input name="activities[]" type="text" />
+            <button class="buttonAddInput" onclick="addInput(this);">
+                <span class="ui-icon ui-icon-circle-plus"></span>
+            </button>
         </div>
         <div>
 	        <label for="description">Description</label><div class="description"><textarea name="description"></textarea></div>
         </div>
-        <?php
-        if (isset($_GET['id']))
-        {
-	        echo "<script type=\"text/javascript\">";
-	        echo "Task.tasks['{$_GET['id']}'].fillEditor();";
-	        echo "</script>";
-        }
-        ?>
+        <div>
+            <input type="submit" value="Edit" />
+        </div>
     </form>
 </div>
+
+<script type="text/javascript">
+    var availableTags = [
+    "ActionScript",
+    "AppleScript",
+    "Asp",
+    "BASIC",
+    "C",
+    "C++",
+    "Clojure",
+    "COBOL",
+    "ColdFusion",
+    "Erlang",
+    "Fortran",
+    "Groovy",
+    "Haskell",
+    "Java",
+    "JavaScript",
+    "Lisp",
+    "Perl",
+    "PHP",
+    "Python",
+    "Ruby",
+    "Scala",
+    "Scheme"
+    ];
+    $( 'input[name*="activities[]"]' ).autocomplete({
+    source: availableTags
+    });
+    <?php
+        if (isset($_GET['id']))
+        {
+	        echo "Task.tasks['{$_GET['id']}'].fillEditor();";
+        }
+    ?>
+</script>
