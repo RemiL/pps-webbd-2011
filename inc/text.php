02642bb120d866cf8dc458169873568cc17f227e
nivet1@hotmail.com <nivet1@hotmail.com@8e272b94-ffaf-4885-f8fc-8a814f68a3e8>
2011-04-26 14:55:37 +0000

    - petite erreur ^^

--- inc/text.php
+++ inc/text.php
@@ -1,7 +1,7 @@
 <div class="contentMenuAction">
     <form name="text" class="text" action="inc/enregistrerTexte.php?id=toto" method="POST">
 		<textarea id="champTexte" name="texte"></textarea>
-		<input type = "Submit" name = "enregistrer" value = "Register">
+		<input type = "Submit" name = "enregistrer" value = "Save">
 		<input type="button" value="Export" onClick="">
 	</form>
 </div>
