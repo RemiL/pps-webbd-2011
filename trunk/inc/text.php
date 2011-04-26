48aa2238ec2efd7275952bbc297c47b94bc23f8a
nivet1@hotmail.com <nivet1@hotmail.com@8e272b94-ffaf-4885-f8fc-8a814f68a3e8>
2011-04-26 14:54:50 +0000

    - Mode anglais activé

--- trunk/inc/text.php
+++ trunk/inc/text.php
@@ -1,7 +1,7 @@
 <div class="contentMenuAction">
     <form name="text" class="text" action="inc/enregistrerTexte.php?id=toto" method="POST">
 		<textarea id="champTexte" name="texte"></textarea>
-		<input type = "Submit" name = "enregistrer" value = "Enregistrer">
-		<input type="button" value="Exporter" onClick="">
+		<input type = "Submit" name = "enregistrer" value = "Register">
+		<input type="button" value="Export" onClick="">
 	</form>
 </div>
