e4d21b74035e70cea3fc81d6cfc8e5dee5a74071
groupeCMT07@gmail.com <groupeCMT07@gmail.com@8e272b94-ffaf-4885-f8fc-8a814f68a3e8>
2011-04-29 21:12:26 +0000

    L'objet postit et panel sont créés La description est remplie à la dérivation d'un postit Le when / due for fontionne parfaitement

--- trunk/js/postit.js
+++ trunk/js/postit.js
@@ -1,124 +1,73 @@
-var panneauPostitOuvert = false;
-var nbPostit = 0;
+function PostIt(panel, body)
+{
+    this.id = PostIt.nbPostit;
+    this.panel = panel;
+    this.body = body;
+    this.text = "";
+    this.valid = false;
+    PostIt.nbPostit++;
+}
 
-function confirmerPostit() {
-    if (document.getElementById('nouveauTextePostit') != null) {
-        var text = document.getElementById('nouveauTextePostit').value;
-        if (text != "") {
-            var zone = document.getElementById('nouveauTextePostit').parentNode;
-            zone.removeChild(document.getElementById('nouveauTextePostit'));
-            zone.removeChild(document.getElementById('collerPostit'));
+PostIt.nbPostit = 0;
 
-            var tabText = text.split('\n');
+PostIt.prototype =
+{
+    save: function ()
+    {
+        if (!this.valid)
+        {
+            var text1 = $('textarea[name="textPostit"]', this.body).val();
 
-            for (var i=0; i<tabText.length; i++)
+            // si le texte est vide, le postit est supprimé
+            if (text1 != "")
             {
-                zone.appendChild(document.createTextNode(tabText[i]));
-                zone.appendChild(document.createElement('br'));
+                this.text = text1;
+
+                // Supprime le formulaire
+                $('textarea', this.body).remove();
+                $('input', this.body).remove();
+
+                // Ajoute le texte
+                var tabText = this.text.split('\n');
+                for (var i = 0; i < tabText.length; i++)
+                {
+                    $('.textPostit', this.body).append(document.createTextNode(tabText[i]));
+                    $('.textPostit', this.body).append(document.createElement('br'));
+                }
+
+                // Ajoute le bouton pour dériver en tâche
+                var deriver = document.createElement('span');
+                deriver.className = 'ui-icon ui-icon-document';
+                deriver.onclick = bind(function () { this.createTask(); }, this);
+                this.body.firstChild.appendChild(deriver);
+
+                // Vérifie la hauteur pour ne pas sortir du cadre
+                var hauteur = 750;
+                var y = this.body.offsetTop + this.body.offsetHeight;
+                if (y > hauteur)
+                {
+                    this.body.style.top = hauteur - this.body.offsetHeight + "px";
+                }
+
+                this.valid = true;
+                this.panel.validPostit();
             }
-
-            var deriver = document.createElement('span');
-            deriver.className = 'ui-icon ui-icon-document';
-            deriver.onclick = function () { deriverPostitTache(zone.parentNode.id); };
-            zone.parentNode.firstChild.appendChild(deriver);
-
-            var postit = document.getElementById('panneauPostit').lastChild;
-            var hauteur = 750;
-            var y = postit.offsetTop + postit.offsetHeight;
-            if (y > hauteur) {
-                postit.style.top = hauteur - postit.offsetHeight + "px";
+            else
+            {
+                this.panel.removePostit(this);
             }
         }
-        else {
-            document.getElementById('panneauPostit').removeChild(document.getElementById('p' + nbPostit));
-        }
-    }
-}
+    },
 
-function addPostit(event) {
-    confirmerPostit();
-    nbPostit++;
-    var y = event.pageY;
-    var x = event.pageX;
-    var largeur = document.body.clientWidth - 4;
-    var hauteur = 748;
-    var postit = document.createElement('div');
-    postit.id = 'p' + nbPostit;
-    postit.className = 'postit ui-corner-all';
+    createTask: function ()
+    {
+        this.panel.removePostit(this);
+        addTab(null, null, this.text);
+        this.panel.toggle();
+    },
 
-    if (y < 150 / 2) {
-        postit.style.top = 0 + "px";
-    }
-    else if (y > hauteur - 150 / 2) {
-        postit.style.top = hauteur - 150 + "px";
-    }
-    else {
-        postit.style.top = event.pageY - 8 - 150 / 2 + "px";
-    }
-
-    if (x < 200 / 2) {
-        postit.style.left = 0 + "px";
-    }
-    else if (x > largeur - 200 / 2) {
-        postit.style.left = largeur - 200 + "px";
-    }
-    else {
-        postit.style.left = event.pageX - 8 - 200 / 2 + "px";
-    }
-
-    var options = document.createElement('div');
-    options.className = 'optionsPostit';
-
-    var supprimer = document.createElement('span');
-    supprimer.className = 'ui-icon ui-icon-closethick';
-    supprimer.onclick = function () { supprimerPostit(postit.id); };
-
-    var zoneText = document.createElement('div');
-    zoneText.className = 'textPostit';
-
-    var textarea = document.createElement('textarea');
-    textarea.id = "nouveauTextePostit";
-    textarea.cols = '13';
-    textarea.rows = '8';
-    var input = document.createElement('input');
-    input.type = 'submit';
-    input.id = 'collerPostit';
-    input.value = 'Coller';
-    input.onclick = function () { confirmerPostit(); };
-
-    options.appendChild(supprimer);
-
-    zoneText.appendChild(textarea);
-    zoneText.appendChild(input);
-
-    postit.appendChild(options);
-    postit.appendChild(zoneText);
-
-    document.getElementById('panneauPostit').appendChild(postit);
-    document.getElementById('nouveauTextePostit').focus();
-
-    $("#" + postit.id).draggable({ containment: 'parent' }, { scroll: false }, { stack: ".postit" });
-}
-
-function supprimerPostit(idPostit) {
-    document.getElementById("panneauPostit").removeChild(document.getElementById(idPostit));
-}
-
-function deriverPostitTache(idPostit) {
-    afficherCacherPanneauPostit();
-    supprimerPostit(idPostit);
-    addTab();
-}
-
-function afficherCacherPanneauPostit() {
-    if (!panneauPostitOuvert) {
-        $("#panneauPostit").switchClass("panneauPostitFerme", "panneauPostitOuvert", 0);
-        panneauPostitOuvert = true;
-        $("#afficherPostit").button("option", "icons", { primary: 'ui-icon-circle-triangle-s' });
-    }
-    else {
-        $("#panneauPostit").switchClass("panneauPostitOuvert", "panneauPostitFerme", 0);
-        panneauPostitOuvert = false;
-        $("#afficherPostit").button("option", "icons", { primary: 'ui-icon-circle-triangle-n' });
+    close: function ()
+    {
+        this.panel.removePostit(this);
     }
 }
